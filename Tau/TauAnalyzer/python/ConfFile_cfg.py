f6532abd0a42bd60990cf297664b44ec1f192f6a
Andrew Issac <aissac@ekplx42.ekp.kit.edu>
2020-10-09 20:54:09 +0200

    Added arguments for condor jobs

--- Tau/TauAnalyzer/python/ConfFile_cfg.py
+++ Tau/TauAnalyzer/python/ConfFile_cfg.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+import sys
 
 process = cms.Process("TAU")
 
@@ -13,15 +14,25 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+# process.source = cms.Source("PoolSource",
+#                                 # replace 'myfile.root' with the source file you want to use
+#                                 fileNames = cms.untracked.vstring(
+#             #'file:/ceph/sbrommer/tau_id/dy_testsample/FEB3954C-4942-E811-8A09-008CFAC91A38.root'
+#             #'/store/mc/RunIIFall17MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/40000/6A62DE01-E641-E811-AFF1-008CFAC94038.root'
+#                 )
+#                             )
+
+arguments_jobID = int(sys.argv[2]) 
+inputfile = str(sys.argv[3]) # this takes the filename of the root file passed in from job.sh from running cmsRun mypath/ConfFile_cfg.py myfile.root (in this case: sys.argv[3] = myfile.root )
+dataset = str(sys.argv[4])
+
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
-                                fileNames = cms.untracked.vstring(
-            'file:/ceph/sbrommer/tau_id/dy_testsample/FEB3954C-4942-E811-8A09-008CFAC91A38.root'
-                )
+                                fileNames = cms.untracked.vstring(inputfile)
                             )
 
 process.tauEDAnalyzer = cms.EDAnalyzer('TauAnalyzer')
 
-process.TFileService = cms.Service( "TFileService", fileName=cms.string("output.root"))
+process.TFileService = cms.Service( "TFileService", fileName=cms.string("output_{:04d}_{}.root".format(arguments_jobID, dataset)))
 
 process.p = cms.Path(process.tauEDAnalyzer)
